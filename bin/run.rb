81c42cfd8c65c431b74d38683e579851f6308e82
Jennifer Gomez <jennifergomezbruno@gmail.com>
2019-12-10 14:33:08 -0500

    So far the gets user input method is working, we have successfully iterated over charaters and we are now trying to iterate through films

--- bin/run.rb
+++ bin/run.rb
@@ -2,6 +2,12 @@
 
 require_relative "../lib/api_communicator.rb"
 require_relative "../lib/command_line_interface.rb"
+require 'JSON'
+require 'rest-client'
+require 'pry'
+
+character_data = RestClient.get('http://swapi.co/api/people/1')
+JSON.parse(character_data)
 
 welcome
 character = get_character_from_user
--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -6,13 +6,21 @@ def get_character_movies_from_api(character_name)
   #make the web request
   response_string = RestClient.get('http://www.swapi.co/api/people/')
   response_hash = JSON.parse(response_string)
-
+  character_array = response_hash["results"]
+  
   # iterate over the response hash to find the collection of `films` for the given
   #   `character`
+    user_character = character_array.find do |character|
+      character["name"].downcase == character_name
+    end
   # collect those film API urls, make a web request to each URL to get the info
   #  for that film
   # return value of this method should be collection of info about each film.
   #  i.e. an array of hashes in which each hash reps a given film
+    # film_url = RestClient.get("https://www.swapi.co/api/films/2/")
+    # films_hash = JSON.parse(film_url)
+    films = []
+
   # this collection will be the argument given to `print_movies`
   #  and that method will do some nice presentation stuff like puts out a list
   #  of movies by title. Have a play around with the puts with other info about a given film.
